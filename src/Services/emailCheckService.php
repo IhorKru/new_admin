a4243dc25ec4d1329e7b937c9c416082fb5e2d2b
Admin <m@mediaff.com>
2018-02-25 02:17:10 +0600

    email validation functionality - commit 5 - closed connection after email check

--- src/Services/emailCheckService.php
+++ src/Services/emailCheckService.php
@@ -77,6 +77,7 @@ class emailCheckService extends PublisherController
             }
             $em->flush();
             $em->clear();
+            $em ->getConnection()->close();
         }
     }
 }
