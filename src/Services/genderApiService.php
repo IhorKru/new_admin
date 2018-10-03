1a6f239f05e8104577c43979f83751042e239cfa
IhorKru <kruchynenko@gmail.com>
2018-10-03 20:57:02 +0600

    fixed gender API

--- src/Services/genderApiService.php
+++ src/Services/genderApiService.php
@@ -12,58 +12,71 @@ use GenderApi\Client as GenderApiClient;
 use GenderApi\GenderApi;
 
 class genderApiService extends PublisherController {
-private $numchecks;
-public $gender_id;
-public function genderApiServiceAction($numchecks) {
-    //select distinct first names from the table according to the count selected
-    $em = $this ->getDoctrine() ->getManager();
-    $subscriber = new SubscriberDetails();
-    //https://gender-api.com/en/
-    $apiClient = new GenderApiClient('enRaeoVEKdoyDFBCkr');
-    //https://genderapi.io/
-    $genderApi = new GenderApi('5bb09bbaff8c27498f6c2601');
-    /* 1. Creating sub batches */
-    $batcharray = array();
-    /* selecting batch size based on the overall count */
-    $batchsize = 10;
-    /* identifiing number of batches */
-    if ($numchecks > $batchsize) {
-        $cntbatch = round($numchecks/$batchsize, 0, PHP_ROUND_HALF_DOWN);
-        $rmd = $numchecks % $batchsize;
-        for ($x = 0; $x < $cntbatch; $x++) {
-            array_push($batcharray, $batchsize);
+    private $numchecks;
+    public $gender_id;
+    public function genderApiServiceAction($numchecks) {
+        //select distinct first names from the table according to the count selected
+        $em = $this ->getDoctrine() ->getManager();
+        $subscriber = new SubscriberDetails();
+        //https://gender-api.com/en/
+        $apiClient = new GenderApiClient('enRaeoVEKdoyDFBCkr');
+        //https://genderapi.io/
+        $genderApi = new GenderApi('5bb09bbaff8c27498f6c2601');
+        /* 1. Creating sub batches */
+        $batcharray = array();
+        /* selecting batch size based on the overall count */
+        $batchsize = 10;
+        /* identifiing number of batches */
+        if ($numchecks > $batchsize) {
+            $cntbatch = round($numchecks/$batchsize, 0, PHP_ROUND_HALF_DOWN);
+            $rmd = $numchecks % $batchsize;
+            for ($x = 0; $x < $cntbatch; $x++) {
+                array_push($batcharray, $batchsize);
+            }
+            array_push($batcharray, $rmd);
+        } else {
+            $cntbatch = 1;
+            $batchsize = $numchecks;
+            for ($x = 0; $x < $cntbatch; $x++) {
+                array_push($batcharray, $batchsize);
+            }
         }
-        array_push($batcharray, $rmd);
-    } else {
-        $cntbatch = 1;
-        $batchsize = $numchecks;
-        for ($x = 0; $x < $cntbatch; $x++) {
-            array_push($batcharray, $batchsize);
-        }
-    }
-    //checking names
-    foreach ($batcharray as $sizecnt) {
-        unset($subscribers);
-        $subscribers = $this->getDoctrine()->getRepository('App:GenderName')->genderCheck($sizecnt);
-        foreach ($subscribers as $subscriber) {
-            $firstname = $subscriber->getFirstname();
-            $lastname = $subscriber->getLastname();
-            //https://gender-api.com/en/
-            try {
-                $lookup = $apiClient->getByFirstNameAndLastNameAndCountry($firstname . ' ' . $lastname, 'US');
-                $returnedgender = $lookup->getGender();
-                $vendor = 'gender-api.com';
-            } catch (GenderApi\Exception $e) {
-                //https://genderapi.io/
+        //checking names
+        foreach ($batcharray as $sizecnt) {
+            unset($subscribers);
+            $subscribers = $this->getDoctrine()->getRepository('App:GenderName')->genderCheck($sizecnt);
+            foreach ($subscribers as $subscriber) {
+                $firstname = $subscriber->getFirstname();
+                $lastname = $subscriber->getLastname();
+                //https://gender-api.com/en/
                 try {
-                    $getResult = $genderApi->result($firstname);
-                    $returnedgender = $getResult->{'gender'};
-                    $vendor = 'genderapi.io';
-                } catch (GenderApi2\Exception $e2) {
-                    //store.genderize.io
-                    echo 'Exception: ' . $e2 ->getMessage();
+                    //check if we still have limit with vendor
+                    $stats = $apiClient->getStats();
+                    // Check your query limit
+                    if (!$stats->isLimitReached()) {
+                        $lookup = $apiClient->getByFirstNameAndLastNameAndCountry($firstname . ' ' . $lastname, 'US');
+                        $returnedgender = $lookup->getGender();
+                        $vendor = 'gender-api.com';
+                    } else {
+                        //https://genderapi.io/
+                        //check if the limit is there for vendor
+                        try {
+                            $getResult = $genderApi->result($firstname);
+                            $errnum = $getResult->{'errno'};
+                            if(!$errnum) {
+                                $returnedgender = $getResult->{'gender'};
+                                $vendor = 'genderapi.io';
+                            } elseif ($errnum == '93') {
+                                die;
+                            }
+                        } catch (GenderApi\Exception $e2) {
+                            //store.genderize.io
+                            echo 'Exception: ' . $e2 ->getMessage();
+                        }
+                    }
+                } catch (GenderApi\Exception $e) {
+                    echo 'Exception: ' . $e->getMessage();
                 }
-                /*}*/
                 $namegender = new GenderName();
                 if ($returnedgender == 'male') {
                     $gender_id = 1;
